7a2f15d682fe10525ec1ecf013cf42e0e4ba05f8
hp <tayyab102003@gmail.com>
2025-02-09 09:10:56 +0500

    Params added in code

--- src/model_building.py
+++ src/model_building.py
@@ -120,7 +120,10 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators': 25, 'random_state': 2}
+        params=load_params(params_path='params.yaml')['model_building']
+        
+
+        # params = {'n_estimators': 25, 'random_state': 2}
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
